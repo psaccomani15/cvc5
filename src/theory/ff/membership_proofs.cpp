1e0a4bac79bdb2ad706b9933b50e1c875607688d
Pedro Saccomani <psaccomani15@gmail.com>
2024-04-17 12:54:32 -0300

    add cocoa hooks for proof production

---
+++ src/theory/ff/membership_proofs.cpp
@@ -0,0 +1,209 @@
+#include "theory/ff/gbProofs.h"
+#include <CoCoA/TmpGPoly.H>
+
+#include <sstream>
+
+#include "smt/assertions.h"
+#include "proof/proof.h"
+#include "util/rational.h"
+#include <CoCoA/SparsePolyOps-ideal.H>
+#include <CoCoA/SparsePolyRing.H>
+
+namespace cvc5::internal {
+namespace theory {
+namespace ff {
+  
+template <typename T>
+std::string ostring(const T& t)
+{
+  std::ostringstream o;
+  o << t;
+  return o.str();
+}
+
+// TODO string to node, so that same string yields the same node
+
+  GBProof::GBProof(const std::vector<CoCoA::RingElem> polys, Node ideal, CoCoA::ideal internalIdeal, CDProof* proof):
+    d_ideal(ideal),
+    cocoaIdeal(internalIdeal),
+    d_proof(proof)
+ 
+{
+  NodeManager* nm = NodeManager::currentNM();;
+
+  for (CoCoA::ConstRefRingElem cPoly : polys)
+  {
+    std::string poly = ostring(cPoly);
+    Node polyRepr = nm->mkBoundVar(poly, nm->sExprType());
+    Node membershipRepr = nm->mkNode(Kind::SEXPR, polyRepr, d_ideal); 
+    std::vector<Node> argsIdeal{polyRepr, d_ideal};
+    Trace("ff::trace") << "..will create pf step " << ProofRule::FF_G << ": "
+		       << membershipRepr << " " << argsIdeal << "\n";
+    d_proof->addStep(membershipRepr, ProofRule::FF_G, {}, argsIdeal);
+    d_polyToMembership.emplace(poly, membershipRepr);
+  }
+  Node zeroConclusion = produceMembershipNode("0", nm);
+  d_proof->addStep(zeroConclusion, ProofRule::FF_Z, {}, {d_ideal});
+};
+
+void GBProof::setFunctionPointers()
+{
+  GBProof* t = this;
+  d_sPoly =
+      std::function([=](CoCoA::ConstRefRingElem p,
+                        CoCoA::ConstRefRingElem q,
+                        CoCoA::ConstRefRingElem s) { t->sPoly(p, q, s); });
+  d_reductionStart =
+      std::function([=](CoCoA::ConstRefRingElem p) { t->reductionStart(p); });
+  d_reductionStep =
+      std::function([=](CoCoA::ConstRefRingElem p) { t->reductionStep(p); });
+  d_reductionEnd =
+      std::function([=](CoCoA::ConstRefRingElem p) { t->reductionEnd(p); });
+  d_membershipStart = std::function([=](CoCoA::ConstRefRingElem p){ t->membershipStart(p); });
+  d_membershipStep = std::function([=](CoCoA::ConstRefRingElem p) {});
+  CoCoA::proofEnabled = true;
+  CoCoA::sPolyProof = d_sPoly;
+  CoCoA::reductionStartProof = d_reductionStart;
+  CoCoA::reductionStepProof = d_reductionStep;
+  CoCoA::reductionEndProof = d_reductionEnd;
+}
+  Node GBProof::produceMembershipNode(std::string poly, NodeManager *nm) {
+  Node polyRepr = nm->mkBoundVar(poly, nm->sExprType()); 
+    return nm->mkNode(Kind::SEXPR, polyRepr, d_ideal);
+    
+}
+Node GBProof::getMembershipFact(CoCoA::ConstRefRingElem poly)
+{
+  std::string polyStrRepr = ostring(poly);
+  Assert(d_polyToMembership.count(polyStrRepr));
+  return d_polyToMembership[polyStrRepr]; 
+}
+  Node GBProof::proofIdealMembership(CoCoA::ConstRefRingElem poly) {
+    std::string polyRepr = ostring(poly);
+    if(d_polyToMembership.count(polyRepr))
+      return getMembershipFact(poly);
+    Assert(CoCoA::HasGBasis(cocoaIdeal));
+    bool hasElem = CoCoA::IsElem(poly, cocoaIdeal);
+    Assert(hasElem);
+    return d_polyToMembership[polyRepr];
+
+}
+
+  
+void GBProof::sPoly(CoCoA::ConstRefRingElem p,
+                              CoCoA::ConstRefRingElem q,
+                              CoCoA::ConstRefRingElem s)
+{
+
+  std::string ss = ostring(s);
+  Trace("ff::trace") << "s: " << p << ", " << q << " -> " << s << std::endl;
+  if (d_polyToMembership.count(ss) == 0)
+  {
+    NodeManager *nm = NodeManager::currentNM();
+    Trace("ff::trace") << " keep" << std::endl;
+    std::string sPoly = ostring(s); 
+    Node conclusion = produceMembershipNode(sPoly, nm); 
+    d_polyToMembership.emplace(sPoly, conclusion); 
+    Node pMNode = d_polyToMembership[ostring(p)];
+    Node qMNode = d_polyToMembership[ostring(q)];
+    std::vector<Node> premises{ pMNode, qMNode};
+    Trace("ff::trace") << "..will create pf step " << ProofRule::FF_S << ": "
+                       << premises << " ---> " << conclusion << "\n";
+
+    // forces the CDProof to check wheter there is a proof for each premise
+    d_proof->addStep(conclusion, ProofRule::FF_S, premises, {}, true);
+  }
+  else
+  {
+    Trace("ff::trace") << " drop" << std::endl;
+  }
+}
+
+ 
+void GBProof::reductionStart(CoCoA::ConstRefRingElem p)
+{
+  Assert(d_reductionSeq.empty());
+  Trace("ff::trace") << "GBreduction start: " << p << std::endl;
+  d_reductionSeq.push_back(ostring(p));
+}
+
+
+// q is the reducer, we then assert that q already has a membership proof.  
+void GBProof::reductionStep(CoCoA::ConstRefRingElem q)
+{
+  Assert(!d_reductionSeq.empty());
+  Trace("ff::trace") << "GBreduction step: " << q << std::endl;
+  Assert(!d_polyToMembership.count(ostring(q)));
+  d_reductionSeq.push_back(ostring(q));
+}
+
+void GBProof::reductionEnd(CoCoA::ConstRefRingElem r)
+{
+  Assert(!d_reductionSeq.empty());
+  NodeManager* nm = NodeManager::currentNM(); 
+  Trace("ff::trace") << "reduction end: " << r << std::endl;
+  std::string rr = ostring(r);
+  if (d_polyToMembership.count(rr) == 0 && rr != d_reductionSeq.front())
+  {
+    std::vector<Node> premises{};
+    std::vector<Node> reductorsSeq{};
+    std::unordered_set<std::string> uniquePolys;
+    Trace("ff::trace") << " keep" << std::endl;
+    Node rrPoly = nm->mkBoundVar(rr, nm->sExprType()); 
+    Node conclusion =  nm->mkNode(Kind::SEXPR, rrPoly, d_ideal);
+    d_polyToMembership.emplace(rr, conclusion); 
+    for (std::string poly : d_reductionSeq)
+    {
+      uniquePolys.insert(poly);
+      reductorsSeq.push_back(nm->mkBoundVar(poly, nm->sExprType()));
+    }
+    for (std::string poly : uniquePolys)
+    {
+      premises.push_back(d_polyToMembership[poly]);
+    }
+    d_proof->addStep(conclusion, ProofRule::FF_R_DOWN, premises, reductorsSeq, true);
+  }
+  else
+  {
+    if (TraceIsOn("ff::trace"))
+    {
+      Trace("ff::trace") << " drop" << std::endl;
+    }
+  }
+  d_reductionSeq.clear();
+}
+
+void GBProof::membershipStart(CoCoA::ConstRefRingElem p) {
+  Assert(d_membershipSeq.empty()); 
+  d_reducingPoly = ostring(p);
+}
+
+void GBProof::membershipStep(CoCoA::ConstRefRingElem red) {
+  d_membershipSeq.push_back(ostring(red));
+}
+void GBProof::membershipEnd()
+{
+  NodeManager* nm = NodeManager::currentNM();
+  Node conclusion = produceMembershipNode(d_reducingPoly, nm);
+  std::vector<Node> reductorsSeq;
+  std::unordered_set<std::string> uniquePolys;
+  for (std::string p : d_membershipSeq)
+  {
+    reductorsSeq.push_back(nm->mkBoundVar(p, nm->sExprType()));
+    uniquePolys.insert(p);
+  }
+  std::vector<Node> premises{};
+  for (std::string p : uniquePolys)
+  {
+    premises.push_back(d_polyToMembership[p]);
+  }
+  premises.push_back(d_polyToMembership["0"]);
+  d_proof->addStep(
+      conclusion, ProofRule::FF_R_UP, premises, reductorsSeq, true);
+  d_membershipSeq.clear(); 
+}
+}  // namespace ff
+}  // namespace theory
+}  // namespace cvc5::internal
+
+//#endif /* CVC5_USE_COCOA */
---
+++ src/theory/ff/membership_proofs.h
@@ -0,0 +1,158 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Alex Ozdemir
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2023 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Finite fields UNSAT trace construction
+ */
+
+#include "cvc5_private.h"
+
+// #if CVC5_USE_COCOA
+#ifndef CVC5__THEORY__GB__PROOF_H
+#define CVC5__THEORY__GB__PROOF_H
+
+#include <CoCoA/TmpGPoly.H>
+#include <CoCoA/ring.H>
+#include <CoCoA/ideal.H>
+#include <functional>
+#include <unordered_map>
+#include "expr/node.h"
+
+
+namespace cvc5::internal {
+
+class CDProof;
+
+namespace theory {
+namespace ff {
+
+/**
+ * A non-incremental dependency graph for CoCoA polynomials in Groebner basis
+ * computation.
+ *
+ * We represent polynomials as their strings.
+ */
+class GBProof
+{
+  
+ public:
+
+  GBProof(const std::vector<CoCoA::RingElem> polys, Node ideal, CoCoA::ideal cocoaIdeal, CDProof* proof);
+
+  /*
+   * Hooks into CoCoA. The functions are then called by CoCoA Groebner Basis during
+   * Reduction and computation of SPolynomials
+   */  
+  void setFunctionPointers(); 
+  /*
+   * Returns a membership proof for a given polynomial. 
+   * The polynomial must be already registered in d_polyToVar. 
+  */
+  Node getMembershipFact(CoCoA::ConstRefRingElem poly); 
+
+  /*
+   * Produces/uses a membership fact for arbitrary polynomials in the ideal. 
+   * The argument "poly" *must* be an element of the ideal.  
+   */
+  Node proofIdealMembership(CoCoA::ConstRefRingElem poly); 
+private:
+
+  /**
+   * Produces Nodes that represents ideal membership facts.
+   */
+  Node produceMembershipNode(std::string poly, NodeManager *nm);
+
+  /**
+   * Call this when s = spoly(p, q);
+   */
+  void sPoly(CoCoA::ConstRefRingElem p,
+             CoCoA::ConstRefRingElem q,
+             CoCoA::ConstRefRingElem s);
+  /**
+   * Call this when we start reducing p.
+   */
+  void reductionStart(CoCoA::ConstRefRingElem p);
+  /**
+   * Call this when there is a reduction on q.
+   */
+  void reductionStep(CoCoA::ConstRefRingElem q);
+  /**
+   * Call this when we finish reducing with r.
+   */
+  void reductionEnd(CoCoA::ConstRefRingElem r);
+
+  void membershipStart(CoCoA::ConstRefRingElem p);
+
+  void membershipStep(CoCoA::ConstRefRingElem s);
+
+  void membershipEnd(); 
+  /**
+   * For each poly string, its index in the input sequence.
+   */
+  std::unordered_map<std::string, size_t> d_inputNumbers;
+
+  /**
+   * The sequence of polynomials used for reduction during GBasis production. 
+   */
+  std::vector<std::string> d_reductionSeq{};
+
+  /**
+   * The sequence of polynomials in GBasis used in the reduction during independent membership proof production.  
+   */
+  std::vector<std::string> d_membershipSeq{};
+
+  /**
+   * Hooks for: 
+   * Gbasis Proof Production: sPoly, reductionStart, reductionStep, reductionEnd
+   * Arbitrary Poly Proof Production: membershipStart, membershipStep and membershipEnd. 
+   */
+  
+  std::function<void(CoCoA::ConstRefRingElem,
+                  CoCoA::ConstRefRingElem,
+                  CoCoA::ConstRefRingElem)>
+   d_sPoly{};
+  std::function<void(CoCoA::ConstRefRingElem)> d_reductionStart{};
+  std::function<void(CoCoA::ConstRefRingElem)> d_reductionStep{};
+  std::function<void(CoCoA::ConstRefRingElem)> d_reductionEnd{};  
+  std::function<void(CoCoA::ConstRefRingElem)> d_membershipStart{};
+  std::function<void(CoCoA::ConstRefRingElem)> d_membershipStep{};
+  std::function<void(void)> d_membershipEnd{};
+  
+  /**
+   * A representation of the Ideal that we are currently proving membership facts from
+   * An sExpr of bound variables that represents the initial set of generators. 
+   */
+  Node d_ideal;  
+
+  CoCoA::ideal cocoaIdeal;
+
+  /**
+   * Maps polynomials to their original ideal membership proofs 
+   */  
+  std::unordered_map<std::string, Node> d_polyToMembership; 
+  /**
+   * Used for arbitrary membership proofs.
+   * Represents the polynomial that we are currently testing for membership   
+   */
+  std::string d_reducingPoly;
+  /**
+   * The user-context-dependent proof object
+   */
+  CDProof* d_proof;
+};
+
+}  // namespace ff
+}  // namespace theory
+}  // namespace cvc5::internal
+
+#endif /* CVC5__THEORY__FF__PROOF_H */
+
+//#endif /* CVC5_USE_COCOA */
