167602a09331bd282d1b4014e5ea424e37417dd9
psaccomani <psaccomani15@gmail.com>
2025-12-10 16:33:18 -0300

    Add new proof rules

--- src/api/cpp/cvc5_proof_rule_template.cpp
+++ src/api/cpp/cvc5_proof_rule_template.cpp
@@ -203,7 +203,19 @@ const char* toString(ProofRule rule)
       return "ARITH_TRANS_SINE_APPROX_BELOW_NEG";
     case ProofRule::ARITH_TRANS_SINE_APPROX_BELOW_POS:
       return "ARITH_TRANS_SINE_APPROX_BELOW_POS";
-    //================================================= External rules
+    //================================================= Finite fields
+    case ProofRule::FF_ROOT_BRANCH: return "FF_ROOT_BRANCH";
+    case ProofRule::FF_EXHAUST_BRANCH: return "FF_EXHAUST_BRANCH";
+    case ProofRule::FF_FIELD_POLYS: return "FF_FIELD_POLYS";
+    case ProofRule::FF_ONE_UNSAT: return "FF_ONE_UNSAT";
+    case ProofRule::FF_POLY_CONVERSION: return "FF_POLY_CONVERSION";
+    case ProofRule::FF_IDEAL_ZERO: return "FF_IDEAL_ZERO";
+    case ProofRule::FF_IDEAL_GENERATOR: return "FF_IDEAL_GENERATOR";
+    case ProofRule::FF_IDEAL_REDUCE_ZERO: return "F_IDEAL_REDUCE_ZERO";
+    case ProofRule::FF_IDEAL_REDUCE: return "FF_IDEAL_REDUCE";
+    case ProofRule::FF_IDEAL_SPOLY: return "FF_IDEAL_SPOLY";
+    case ProofRule::FF_IDEAL_MONIC: return "FF_IDEAL_MONIC";
+   //================================================= External rules
     case ProofRule::LFSC_RULE: return "LFSC_RULE";
     case ProofRule::ALETHE_RULE: return "ALETHE_RULE";
     //================================================= Unknown rule
@@ -211,7 +223,6 @@ const char* toString(ProofRule rule)
     default: return "?";
   }
 }
-
 std::ostream& operator<<(std::ostream& out, ProofRule rule)
 {
   out << toString(rule);
@@ -230,7 +241,8 @@ const char* toString(cvc5::ProofRewriteRule rule)
     case ProofRewriteRule::UBV_TO_INT_ELIM: return "ubv-to-int-elim";
     case ProofRewriteRule::INT_TO_BV_ELIM: return "int-to-bv-elim";
     case ProofRewriteRule::MACRO_BOOL_NNF_NORM: return "macro-bool-nnf-norm";
-    case ProofRewriteRule::MACRO_BOOL_BV_INVERT_SOLVE: return "macro-bool-bv-invert-solve";
+    case ProofRewriteRule::MACRO_BOOL_BV_INVERT_SOLVE:
+      return "macro-bool-bv-invert-solve";
     case ProofRewriteRule::MACRO_ARITH_INT_EQ_CONFLICT:
       return "macro-arith-int-eq-conflict";
     case ProofRewriteRule::MACRO_ARITH_INT_GEQ_TIGHTEN:
@@ -249,7 +261,8 @@ const char* toString(cvc5::ProofRewriteRule rule)
     case ProofRewriteRule::MACRO_LAMBDA_CAPTURE_AVOID:
       return "macro-lambda-capture-avoid";
     case ProofRewriteRule::ARRAYS_SELECT_CONST: return "arrays-select-const";
-    case ProofRewriteRule::MACRO_ARRAYS_NORMALIZE_OP: return "macro-arrays-normalize-op";
+    case ProofRewriteRule::MACRO_ARRAYS_NORMALIZE_OP:
+      return "macro-arrays-normalize-op";
     case ProofRewriteRule::MACRO_ARRAYS_NORMALIZE_CONSTANT:
       return "macro-arrays-normalize-constant";
     case ProofRewriteRule::ARRAYS_EQ_RANGE_EXPAND:
